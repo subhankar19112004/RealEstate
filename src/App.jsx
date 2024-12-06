4f6440fea0b564b59e55560bcecadd0115ef6ad4
subhankar19112004 <subhankarjenaofficial19@gmail.com>
2024-12-06 07:25:20 +0530

    industries

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import "./App.css";
+import Companies from "./components/Companies/Companies";
+import Contact from "./components/contacts/Contacts";
+
+import Footer from "./components/Footer/Footer";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
+    </div>
   );
 }
 
